6376ad177c70898e9cb359ffd1d112c804405b6d
jihwan <jihwan1535@nate.com>
2024-05-03 03:29:19 +0900

    chore : project init

---
+++ src/main/java/com/test/oepnai/OepnAiApplication.java
@@ -0,0 +1,13 @@
+package com.test.oepnai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OepnAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OepnAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/oepnai/OepnAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.oepnai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OepnAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
